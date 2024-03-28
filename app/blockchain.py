7c61e046f4c3c2cc6f79c8ba78f465312224b893
dylan tirandaz <93934418+dylantirandaz@users.noreply.github.com>
2024-03-27 22:34:00 -0500

    Update blockchain.py

--- app/blockchain.py
+++ app/blockchain.py
@@ -1,1 +1,43 @@
+import time
+import json
+from hashlib import sha256
 
+class Block:
+    def __init__(self, index, timestamp, votes, previous_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.votes = votes
+        self.previous_hash = previous_hash
+        self.nonce = 0
+
+    def compute_hash(self):
+        block_string = json.dumps(self.__dict__, sort_keys=True)
+        return sha256(block_string.encode()).hexdigest()
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.create_genesis_block()
+
+    def create_genesis_block(self):
+        genesis_block = Block(0, time.time(), {}, "0")
+        genesis_block.hash = genesis_block.compute_hash()
+        self.chain.append(genesis_block)
+
+    def add_block(self, block):
+        block.previous_hash = self.chain[-1].hash
+        block.hash = block.compute_hash()
+        self.chain.append(block)
+
+    def is_valid(self):
+        for i in range(1, len(self.chain)):
+            current_block = self.chain[i]
+            previous_block = self.chain[i - 1]
+
+            if current_block.hash != current_block.compute_hash():
+                return False
+
+            if current_block.previous_hash != previous_block.hash:
+                return False
+
+        return True
