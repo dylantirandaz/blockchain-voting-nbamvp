e24bc06b9c0a7eddc0cd8eb5532466b967873beb
dylan tirandaz <93934418+dylantirandaz@users.noreply.github.com>
2024-03-27 22:34:13 -0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,1 +1,17 @@
+from app import db
+from .models import Vote
 
+def add_vote(candidate):
+    vote = Vote(candidate=candidate)
+    db.session.add(vote)
+    db.session.commit()
+
+def get_votes():
+    votes = Vote.query.all()
+    vote_count = {}
+    for vote in votes:
+        if vote.candidate in vote_count:
+            vote_count[vote.candidate] += 1
+        else:
+            vote_count[vote.candidate] = 1
+    return vote_count
